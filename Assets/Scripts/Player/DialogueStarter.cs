0d75349212ce5d283fa85d6114a4cbe78739408e
Max <48150531+mashtakovmm@users.noreply.github.com>
2024-03-07 21:18:01 +0300

    add highlight for interactable objects

--- Assets/Scripts/Player/DialogueStarter.cs
+++ Assets/Scripts/Player/DialogueStarter.cs
@@ -6,6 +6,8 @@ public class DialogueStarter : MonoBehaviour
     private PlayerInputActions playerInputActions;
     private Camera playerCamera;
     [SerializeField] private float raycastDistance = 3f;
+    private GameObject prevHitObject = null;
+    private Color defaultColor;
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
@@ -20,8 +22,27 @@ public class DialogueStarter : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, raycastDistance))
         {
-
+            GameObject hitObject = hit.collider.gameObject;
+            Debug.Log(hitObject == prevHitObject);
+            if (prevHitObject != hitObject)
+            {
+                if (prevHitObject != null)
+                {
+                    prevHitObject.GetComponent<Renderer>().material.SetColor("_Color", defaultColor); // Color.cyan
+                }
+                defaultColor = hitObject.GetComponent<Renderer>().material.GetColor("_Color");
+                prevHitObject = hitObject;
+                if (hitObject.GetComponent<DialogueHaver>())
+                {
+                    hitObject.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                }
+            }
         }
+        else if (prevHitObject)
+            {
+                prevHitObject.GetComponent<Renderer>().material.SetColor("_Color", defaultColor); // Color.cyan
+                prevHitObject = null;
+            }
     }
 
     private void OnEnable()
